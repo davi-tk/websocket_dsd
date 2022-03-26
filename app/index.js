d56cd804e30655f84b23ae9d54e8c1429d922803
Davi Dantas <davi.soares02@gmail.com>
2022-03-26 11:59:10 -0300

    finalizando aplicação

--- app/index.js
+++ app/index.js
@@ -6,9 +6,18 @@ socket.on('message', text => {
     document.querySelector('ul').appendChild(li)
 })
 
+document.querySelector('.btn-nome').onclick = () => {
+    
+    document.querySelector('.login').style.display = 'none'
+    const nome = document.querySelector('.input-nome').value
+    console.log(`nome = ${nome}`)
+    socket.emit('name', nome)
+
+}
+
 document.getElementById('btn-enviar-mensagem').onclick = () => {
     input = document.getElementById('input-mensagem')
     const text = input.value
     input.value = ''
     socket.emit('message', text)
-} 
+}
--- server/index.js
+++ server/index.js
@@ -4,13 +4,22 @@ const io = require('socket.io')(http, {
     cors: {origin: '*'}
 })
 
+const nomes = {}
+
 io.on('connection', (socket) => {
     console.log('usuário conectado')
 
     socket.on('message', message => {
         console.log(message)
-        io.emit('message', `${socket.id.substr(0,2)} -> ${message}`)
+        io.emit('message', `${nomes[socket.id.substr(0,7)]} -> ${message}`)
+    })
+
+    socket.on('name', nome => {
+        console.log(nome)
+        nomes[socket.id.substr(0,7)] = nome
     })
 })
+
+
 
 http.listen(8080, () => console.log('escutando em http://localhost:8080'))
